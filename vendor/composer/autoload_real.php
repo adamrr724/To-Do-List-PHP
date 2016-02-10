5bfda5bf9a7cd5314ae4c83de0b0dd3873c490a8
adam ross russell and jon sweeney <student@epicodus.com>
2016-02-09 16:34:55 -0800

    added templed twig files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInitf15fafa63382c5ab0f6cda1ddeb5e31e
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitf15fafa63382c5ab0f6cda1ddeb5e31e', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
